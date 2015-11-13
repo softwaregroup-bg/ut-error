4c89ebc91ccf8d05483c963ce7abc41618c22148
Stamen Peev <stamen.peev@softwaregroup-bg.com>
2015-11-14 00:23:45 +0200

    Tests improvements

--- test/index.js
+++ test/index.js
@@ -1,127 +1,103 @@
 var utError = require('ut-error');
 
-var errorType1 = utError.define('ErrorType1');
-var errorType2 = utError.define('ErrorType2', 'ErrorType1');
-var errorType3 = utError.define('ErrorType3', 'ErrorType2');
+var errorTypes = {
+	errorType1 : utError.define('ErrorType1'),
+	errorType2 : utError.define('ErrorType2', 'ErrorType1'),
+	errorType3 : utError.define('ErrorType3', 'ErrorType2')
+}
 
-var testErrors = {
+var errorInstances = {
     errorType1 : {
-        noArgs                  : errorType1.generate(),
-        stringArg               : errorType1.generate('errorType1 custom stringArg message'),
-        objectArg               : errorType1.generate({code: 1, message: 'errorType1 custom objectArg message'}),
-        objectArgInterpolation  : errorType1.generate({message: 'errorType1 custom {test} objectArg message', params: {test: 'interpolated'}}),
-        jsExceptionArg          : errorType1.generate(new Error('errorType1 real exception'))
+        noArgs                  : errorTypes.errorType1(),
+        stringArg               : errorTypes.errorType1('errorType1 custom stringArg message'),
+        objectArg               : errorTypes.errorType1({code: 1, message: 'errorType1 custom objectArg message'}),
+        objectArgInterpolation  : errorTypes.errorType1({message: 'errorType1 custom {test} objectArg message', params: {test: 'interpolated'}}),
+        jsExceptionArg          : errorTypes.errorType1(new Error('errorType1 real exception'))
     },
     errorType2 : {
-        noArgs                  : errorType2.generate(),
-        stringArg               : errorType2.generate('errorType2 custom stringArg message'),
-        objectArg               : errorType2.generate({code: 1, message: 'errorType2 custom objectArg message'}),
-        objectArgInterpolation  : errorType2.generate({message: 'errorType2 custom {test} objectArg message', params: {test: 'interpolated'}}),
-        jsExceptionArg          : errorType2.generate(new Error('errorType2 real exception'))
+        noArgs                  : errorTypes.errorType2(),
+        stringArg               : errorTypes.errorType2('errorType2 custom stringArg message'),
+        objectArg               : errorTypes.errorType2({code: 1, message: 'errorType2 custom objectArg message'}),
+        objectArgInterpolation  : errorTypes.errorType2({message: 'errorType2 custom {test} objectArg message', params: {test: 'interpolated'}}),
+        jsExceptionArg          : errorTypes.errorType2(new Error('errorType2 real exception'))
     },
     errorType3 : {
-        noArgs                  : errorType3.generate(),
-        stringArg               : errorType3.generate('errorType3 custom stringArg message'),
-        objectArg               : errorType3.generate({code: 1, message: 'errorType3 custom objectArg message'}),
-        objectArgInterpolation  : errorType3.generate({message: 'errorType3 custom {test} objectArg message', params: {test: 'interpolated'}}),
-        jsExceptionArg          : errorType3.generate(new Error('errorType3 real exception'))
+        noArgs                  : errorTypes.errorType3(),
+        stringArg               : errorTypes.errorType3('errorType3 custom stringArg message'),
+        objectArg               : errorTypes.errorType3({code: 1, message: 'errorType3 custom objectArg message'}),
+        objectArgInterpolation  : errorTypes.errorType3({message: 'errorType3 custom {test} objectArg message', params: {test: 'interpolated'}}),
+        jsExceptionArg          : errorTypes.errorType3(new Error('errorType3 real exception'))
     }
 }
 
 // inspect properties
-for (var errorType in testErrors) {
-    if (testErrors.hasOwnProperty(errorType)) {
-        for (var err in testErrors[errorType]) {
-            if (testErrors[errorType].hasOwnProperty(err)) {
+for (var errorType in errorInstances) {
+    if (errorInstances.hasOwnProperty(errorType)) {
+        for (var err in errorInstances[errorType]) {
+            if (errorInstances[errorType].hasOwnProperty(err)) {
                 console.log('\nError Type: ', errorType);
                 console.log('Instance Name: ', err);
-                console.log('Error Message: ', testErrors[errorType][err].message);
+                console.log('Error Message: ', errorInstances[errorType][err].message);
                 var props = {};
-                for (var prop in testErrors[errorType][err]) {
-                    if (testErrors[errorType][err].hasOwnProperty(prop)) {
-                        props[prop] = testErrors[errorType][err][prop];
+                for (var prop in errorInstances[errorType][err]) {
+                    if (errorInstances[errorType][err].hasOwnProperty(prop)) {
+                        props[prop] = errorInstances[errorType][err][prop];
                     }
                 }
                 console.log('Error Properties: ', JSON.stringify(props, null, 4), '\n');
-                console.log('stack: ', testErrors[errorType][err].stack);
+                console.log('Error stack: ', errorInstances[errorType][err].stack);
                 console.log('\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n');
             }
         }
     }
 }
 
-console.log('\n\nInheritance Tests \n\n')
+console.log('\n\nSuccessfull Inheritance Tests \n\n');
 
-console.log(
-    'testErrors.errorType1.objectArg instanceof Error',
-    '      : ',
-    testErrors.errorType1.objectArg instanceof Error
-);
+for(var errorType in errorInstances) {
+    if (errorInstances.hasOwnProperty(errorType)) {
+        for (var err in errorInstances[errorType]) {
+            if (errorInstances[errorType].hasOwnProperty(err)) {
+                console.log(
+                    (errorInstances[errorType][err] instanceof Error) +
+                    ' <- '+
+                    'errorInstances.' + errorType + '.' + err + ' instaneof Error'
+                );
+                for (var errType in errorTypes) {
+                	if (errorTypes.hasOwnProperty(errType)) {
+                		if(parseInt(errorType.slice(-1)) >= parseInt(errType.slice(-1))) {
+                			console.log(
+				                (errorInstances[errorType][err] instanceof errorTypes[errType]) +
+				                ' <- '+
+				                'errorInstances.' + errorType + '.' + err + ' instaneof errorTypes.' + errType
+				            );
+                		}
+                	}
+                }
+            }
+        }
+    }
+}
 
-console.log(
-    'testErrors.errorType1.objectArg instanceof ErrorType1',
-    ' : ',
-    testErrors.errorType1.objectArg instanceof errorType1.getConstructor()
-);
 
-console.log(
-    'testErrors.errorType2.objectArg instanceof Error',
-    '      : ',
-    testErrors.errorType2.objectArg instanceof Error
-);
+console.log('\n\nUnsuccessfull Inheritance Tests \n\n');
 
-console.log(
-    'testErrors.errorType2.objectArg instanceof ErrorType1',
-    ' : ',
-    testErrors.errorType2.objectArg instanceof errorType1.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType2.objectArg instanceof ErrorType2',
-    ' : ',
-    testErrors.errorType2.objectArg instanceof errorType2.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType3.objectArg instanceof Error',
-    '      : ',
-    testErrors.errorType3.objectArg instanceof Error
-);
-
-console.log(
-    'testErrors.errorType3.objectArg instanceof ErrorType1',
-    ' : ',
-    testErrors.errorType3.objectArg instanceof errorType1.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType3.objectArg instanceof ErrorType2',
-    ' : ',
-    testErrors.errorType3.objectArg instanceof errorType2.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType3.objectArg instanceof ErrorType3',
-    ' : ',
-    testErrors.errorType3.objectArg instanceof errorType3.getConstructor()
-);
-
-console.log('--------------');
-
-console.log(
-    'testErrors.errorType1.objectArg instanceof ErrorType2',
-    ' : ',
-    testErrors.errorType1.objectArg instanceof errorType2.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType2.objectArg instanceof ErrorType3',
-    ' : ',
-    testErrors.errorType2.objectArg instanceof errorType3.getConstructor()
-);
-
-console.log(
-    'testErrors.errorType1.objectArg instanceof ErrorType3',
-    ' : ',
-    testErrors.errorType1.objectArg instanceof errorType3.getConstructor()
-);
+for(var errorType in errorInstances) {
+    if (errorInstances.hasOwnProperty(errorType)) {
+        for (var err in errorInstances[errorType]) {
+            if (errorInstances[errorType].hasOwnProperty(err)) {
+                for (var errType in errorTypes) {
+                	if (errorTypes.hasOwnProperty(errType)) {
+                		if(parseInt(errorType.slice(-1)) < parseInt(errType.slice(-1))) {
+                			console.log(
+				                (errorInstances[errorType][err] instanceof errorTypes[errType]) +
+				                ' <- '+
+				                'errorInstances.' + errorType + '.' + err + ' instaneof errorTypes.' + errType
+				            );
+                		}
+                	}
+                }
+            }
+        }
+    }
+}
